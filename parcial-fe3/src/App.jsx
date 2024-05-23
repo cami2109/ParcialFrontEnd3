6a7366ddfda3295541101a99683d0fac9ba0506b
CamiVg <44098210+cami2109@users.noreply.github.com>
2024-05-22 21:00:00 -0300

    Update App.jsx

--- parcial-fe3/src/App.jsx
+++ parcial-fe3/src/App.jsx
@@ -1,1 +1,14 @@
+import './App.css'
+import Card from './Componentes/Card'
 
+function App() {
+  return (
+    <div className="App" >
+    <h1> Carga de estudiantes</h1>
+            <Card />
+            <form></form>
+    </div>
+
+  
+  );
+}
