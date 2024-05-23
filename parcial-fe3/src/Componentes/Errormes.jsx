c2358bea8aeb09ad03d568afd8b98ff9ca379bfb
CamiVg <44098210+cami2109@users.noreply.github.com>
2024-05-22 22:33:57 -0300

    Update Errormes.jsx

---
+++ parcial-fe3/src/Componentes/Errormes.jsx
@@ -0,0 +1,11 @@
+
+const Errormes = () => {
+  return (
+    <div>
+    <h3 style={{color:"red"}}>
+        Por favor chequea que la informacion sea correcta 
+    </h3>
+    </div>
+  )
+}
+export default Errormes;
